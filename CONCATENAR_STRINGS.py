62866bcf958d550938dfa73aeae83800f2e87e8d
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-08-22 00:48:57 -0500

    Practica_3

---
+++ CONCATENAR_STRINGS.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ coding: 850 _*_
+# _*_ coding: utf-8 _*_
+
+grupo = "Hola" + " " + "pepe"
+print(grupo)
+
+pa1 = "Hola"
+pa2 = "pepe"
+grupo2= pa1 + pa2
+
+pa1 = "Hola"
+print(pa1 + " " + "Luis")
+
+nom = "Juan Pablo"
+ap1 = "Mayoral"
+ap2 = "López"
+nombre = nom + " " + ap1 + " " + ap2
+print(nombre)
+
+print("21" + " y " + "8")
+
+
