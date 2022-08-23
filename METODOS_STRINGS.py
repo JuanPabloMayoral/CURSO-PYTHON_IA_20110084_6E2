18f385d0e65df7408900e64db31f440b551bc21d
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-08-23 11:54:31 -0500

    Prcatica_4

---
+++ METODOS_STRINGS.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ coding: 850 _*_
+# _*_ coding: utf-8 _*_
+
+nombre = "enrique barros fernández"
+print(nombre.title())
+
+frase = "Esta Es Una Frase Para Ser Formateada"
+print(frase.lower())
+
+frase2 = "Esta Es Una Frase Para Ser Formateada"
+print(frase2.upper())
