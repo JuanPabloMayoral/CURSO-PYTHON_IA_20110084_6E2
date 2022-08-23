374b63b6d3c45ace0bdaf7601a84422c74ecd8be
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-08-23 12:07:56 -0500

    Practica_5

---
+++ SALTOS_Y_TABULACIONES_STRINGS.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ coding: 850 _*_
+# _*_ coding: utf-8 _*_
+
+print("Lenguajes de Programación:\n\n\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.\n\t-Kotlin.\n\t-Swift.")
