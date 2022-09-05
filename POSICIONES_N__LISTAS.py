9576c253124a1ad700752be938b550b85148edb0
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:36:20 -0500

    Practica_11

---
+++ POSICIONES_N__LISTAS.py
@@ -0,0 +1,26 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 11 - Acceder a las posiciones negativas de python
+#Utiliza las posiciones negativas para acceder e imprimir algunos de los colores de esta lista. Los colores a los que tienes que acceder son 'naranja', 'amarillo', 'lila', 'blanco' y 'rojo'.
+
+print("ACTIVIDAD:")
+print("Lista: rojo, azul, verde, amarillo, marron, lila, negro, rosa")
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+# naranja
+print("El color en la posicion -1 es:")
+print(colores[-1])
+
+# amarillo
+print("El color en la posicion -7 es:")
+print(colores[-7])
+
+# lila
+print("El color en la posicion -5 es:")
+print(colores[-5])
+
+# blanco
+print("El color en la posicion -2 es:")
+print(colores[-2])
+
+# rojo
+print("El color en la posicion -10 es:")
+print(colores[-10])
