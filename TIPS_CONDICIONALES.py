5f1ac6987be984d9fb232cc5f7b82e4cc6044107
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:36:52 -0500

    Practica_26

---
+++ TIPS_CONDICIONALES.py
@@ -0,0 +1,23 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 26 - Tips para condicionales en python
+
+print("ACTIVIDAD 1 AND")
+
+x = 100
+y = 200
+z = 300
+
+if x < y and z > y:
+    print("Se cumplen las dos condiciones")
+
+else:
+    print("No se cumplen las condiciones")
+
+print("ACTIVIDAD 2 OR")
+
+if x < y and z > y:
+    print("Se cumplen las dos condiciones")
+
+else:
+    print("No se cumplen las condiciones")
+
