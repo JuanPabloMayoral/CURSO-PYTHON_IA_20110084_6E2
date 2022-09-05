b4348743672a4fa81a62bdc60a8d902ec328f90e
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:34:18 -0500

    Practica_21

---
+++ IF_OP_COMP.py
@@ -0,0 +1,23 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 21 - El condicional IF y operadores de comparación 
+
+print("ACTIVIDAD1")
+num1 = 15
+num2 = 20
+
+if num1 <= num2:
+	print('TRUE')
+
+print("ACTIVIDAD 2")
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('TRUE')
+
+print("ACTIVIDAD 3")
+num1 = 1450
+num2 = 60
+
+if num1 != num2:
+	print('FALSE')
