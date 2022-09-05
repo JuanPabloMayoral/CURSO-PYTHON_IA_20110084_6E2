22bbd14dc448540e8bf345edb4f06ed047e03716
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:37:04 -0500

    Practica_19

---
+++ TUPLAS_LISTAS.py
@@ -0,0 +1,16 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 19 - Crear y manejar tuplas - diferencias con las listas para python
+
+print("ACTIVIDAD")
+print("\nLista de colores: rojo, azul, verde, amarillo, marron, lila, negro, rosa, blanco, naranja")
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron',
+          'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print("\nVamos a imprimir la primera posicion de la lista: ")
+print(colores[1])
+print("\nACTIVIDAD 2")
+numeros = (10, 1, 5, 11)
+print("\nListas de numeros: 10, 1 , 5 , 11 ")
+operacion = numeros[0] + numeros[2] + numeros[3] - numeros[1]
+print("\nVamos hacer la operacion numeros[0] + numeros[2] + numeros[3] - numeros[1] : ")
+print("\nResultado de la operacion: ")
+print(operacion)
