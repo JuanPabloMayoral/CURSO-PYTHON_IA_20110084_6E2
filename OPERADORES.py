3ecb75248e0724b88137779432523f3e2d65e98b
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:35:55 -0500

    Practica_7

---
+++ OPERADORES.py
@@ -0,0 +1,27 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 7 - Suma, resta, multiplicacion y division en python
+print("SUMA")
+print(" 20 + 23 + 44 ")
+operacion1 = 20 + 23 + 44 
+print("Resultado: ", operacion1)
+print("\nRESTA")
+print("20 - 23 - 44")
+operacion2 = 20 - 23 - 44 
+print("Resultado: ", operacion2)
+print("\nMULTIPLICACION")
+print("10 * 87")
+operacion3 = 10 * 87
+print("Resultado: ", operacion3)
+print("\nDIVISION")
+print("5000 / 230 / 2")
+operacion4 = 5000 / 230 / 2
+print("Resultado: ", operacion4)
+print("\nOPERACION")
+print("10 / 5 + 15 - 17")
+operacion5 = 10 / 5 + 15 - 17
+print("Resultado: ", operacion5)
+
+
+
+
+
