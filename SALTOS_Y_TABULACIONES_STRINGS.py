ba634ba23220e3adfc73194695a54b58912dd0f5
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:52:58 -0500

    Practica_6

--- SALTOS_Y_TABULACIONES_STRINGS.py
+++ SALTOS_Y_TABULACIONES_STRINGS.py
@@ -2,5 +2,6 @@
 # _*_ coding: cp1252 _*_
 # _*_ coding: 850 _*_
 # _*_ coding: utf-8 _*_
-
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 6 - Saltos de linea y tabulaciones en python
 print("Lenguajes de Programación:\n\n\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.\n\t-Kotlin.\n\t-Swift.")
