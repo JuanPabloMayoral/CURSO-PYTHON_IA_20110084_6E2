3dca52a0daa60f42edf662406ed4e3efa3f9bd45
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:33:50 -0500

    Practica_42

---
+++ HEREDAR_INIT_POO.py
@@ -0,0 +1,26 @@
+# Juan Pablo Mayoral López 20110084 6E2
+# Capitulo 42 - Que es la herencia de clases python
+
+class Personas:
+    def __init__(datos, nombre, edad):
+     datos.nombre = nombre
+     datos.edad = edad 
+        
+    def muestra_datos(datos):
+      print("El nombre de la persona es: " + datos.nombre, "y tiene", datos.edad)
+
+persona1 = Personas("Juan Pablo", 21)
+
+persona1.muestra_datos()
+
+class Personas_premium(Personas):
+    def __init__(datos, nombre, edad, instagram):
+        Personas.__init__(datos,nombre,edad)
+        datos.instagram = instagram
+    
+    def muestra_datos_premium(datos):
+        print("El nombre de la persona es: " + datos.nombre, "y tiene", datos.edad, "Su instagram es:", datos.instagram)
+
+persona2 = Personas_premium("Ana", 28, "Anna_12")
+
+persona2.muestra_datos_premium()
