539729eefca4ae06594314de50764e14742e6252
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:53:04 -0500

    Practica_3

--- STRINGS.py
+++ STRINGS.py
@@ -1,3 +1,5 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 3 - String en python
 text1 = 'Soy Juan Pablo.'
 text2 = "Pero me dicen mayo."
 frase = '"print()" se utiliza para imprimir valores en la consola.'
