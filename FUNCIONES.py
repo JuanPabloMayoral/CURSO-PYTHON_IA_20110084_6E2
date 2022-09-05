578ee1fee23cd4c343210aaa8f27cd92169bce47
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:33:42 -0500

    Practica_34

---
+++ FUNCIONES.py
@@ -0,0 +1,10 @@
+# Juan Pablo Mayoral López 20110084 6E2
+# Capitulo 34 - Como crear funciones en python
+
+print("ACTIVIDAD")
+def suma(numero1, numero2): # se agregan los argumentos
+	print(numero1 + numero2) #imprimir la suma
+
+suma(10, 20) #se hace la suma de 10 + 20
+suma(20, 30) # se hace la suma 20 + 30
+suma(50000, 7000) # se hace la suma 50000 + 7000
