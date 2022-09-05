544777ce1f0e5bf2565ea59e6c8fd6444e6c1594
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:32:28 -0500

    Practica_31

---
+++ DICCIONARIOS.py
@@ -0,0 +1,18 @@
+# Juan Pablo Mayoral López 20110084 6E2
+# Capitulo 31 - Como crear un diccionario en python
+#Del diccionario teclado2 del capítulo, muestra los elementos Modelo y Precio con presentación en un print(). 
+#El resultado será esto en la consola:
+print("ACTIVIDAD:")
+teclado1 = {
+	'CategorIa': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'CategorIa': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+print('El modelo', teclado2['Modelo'], 'cuesta', teclado2['Precio'], '$.')
