e57a73363d5734c145bc8e12c2f8e510d8e56311
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:34:10 -0500

    Practica_22

---
+++ IF_ELSE.py
@@ -0,0 +1,16 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 22 - El condicional IF ELSE para python
+
+print("ACTIVIDAD")
+#Vamos a corregir el codigo que nos asignan para que hagan bien el if else
+#color = rojo
+#else color == rojo
+#Print "El color es rojo."
+#if color != rojo
+#Print "El color no es rojo."
+
+color = "rojo"
+if color == "rojo":
+    print ("El color es rojo")
+else:
+    print ("El color no es rojo.")
