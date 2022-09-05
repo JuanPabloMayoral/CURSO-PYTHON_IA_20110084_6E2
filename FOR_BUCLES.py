df31f291d7e426e41697aabaa98a30a814055659
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:33:27 -0500

    Practica_29

---
+++ FOR_BUCLES.py
@@ -0,0 +1,10 @@
+# Juan Pablo Mayoral López 20110084 6E2
+# Capitulo 29 - BUCLE FOR EN PYTHON
+# Crea un bucle for que itere la siguiente tupla y muestre una frase como esta en cada iteración: 
+# 'El color es: ' + color + '.'.
+colores = ('rojo', 'azul', 'verde', 'amarillo')
+
+print("ACTIVIDAD")
+for x in colores:
+	print('El color es: ' + x + '.')
+
