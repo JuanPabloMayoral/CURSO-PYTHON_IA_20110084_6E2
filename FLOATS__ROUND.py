df2956d7eba2bfe123c0de87f8dba1d8d317c15a
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:33:20 -0500

    Practica_9

---
+++ FLOATS__ROUND.py
@@ -0,0 +1,8 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 9 - Los floats y el metodo round() en python
+#Redondear el numero 17.567383292929200234 a 5 decimales
+print("REDONDEAR A 5 ESTE NUMERO:")
+print("17.567383292929200234")
+numero = 17.567383292929200234
+print("Numero ya con 5 decimales:")
+print (round(numero,5))
