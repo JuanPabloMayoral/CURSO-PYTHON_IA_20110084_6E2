eb4480b2ae9f445e0ec43f3bba8601bc2a5563f4
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:34:58 -0500

    Practica_18

---
+++ LEN_LISTAS.py
@@ -0,0 +1,9 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 18 - Contar elementos con len() listas en python
+
+print("ACTIVIDAD")
+print("\nLista de colores: rojo, azul, verde, amarillo, marron, lila, negro, rosa, blanco, naranja")
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron',
+          'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print("\nCuantos colores hay en la lista: ")
+print(len(colores))
