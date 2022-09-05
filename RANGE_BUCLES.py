504061514289df8ef312fc0a59976901f918e598
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:36:27 -0500

    Practica_30

---
+++ RANGE_BUCLES.py
@@ -0,0 +1,8 @@
+# Juan Pablo Mayoral López 20110084 6E2
+# Capitulo 30 - El tipo de dato range() con bucle for
+# Crea un bucle for con un range() que vaya desde el valor 7 hasta el valor 700 en saltos de 100. 
+# Basta con que imprimas el valor de cada iteración.
+
+print("ACTIVIDAD")
+for x in range(7, 700, 100):
+	print(x)
