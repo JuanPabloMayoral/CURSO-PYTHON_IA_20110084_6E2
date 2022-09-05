953e86e1a4548c68e20be4ad841bba7aee739241
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:35:04 -0500

    Practica_10

---
+++ LISTAS.py
@@ -0,0 +1,19 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 10 - Como crear listas y utilizarlas en python
+#Saber que color esta en la posicion 3 y crear una lista donde obtengas los valores en ciertas posiciones
+
+print("ACTIVIDAD 1: COLORES")
+print("Lista de Colores: rojo, azul, verde, amarillo, marron, lila, negro, rosa")
+colores = ["rojo", "azul", "verde", "amarillo", "marron", "lila", "negro", "rosa"]
+print("EL COLOR EN LA POSICION 3 ES: ",colores[3])
+print("EL COLOR EN LA POSICION 0 ES: ",colores[0])
+print("EL COLOR EN LA POSICION 7 ES: ",colores[7])
+
+print("\nACTIVIDAD 2: NUMEROS")
+print("Lista de numeros: tres, dos, cinco, cuatro, uno")
+numeros = ["tres", "dos", "cinco", "cuatro", "uno"]
+print("EL COLOR EN LA POSICION 0 ES: ",numeros[0])
+print("EL COLOR EN LA POSICION 1 ES: ",numeros[1])
+print("EL COLOR EN LA POSICION 2 ES: ",numeros[2])
+print("EL COLOR EN LA POSICION 3 ES: ",numeros[3])
+print("EL COLOR EN LA POSICION 4 ES: ",numeros[4])
