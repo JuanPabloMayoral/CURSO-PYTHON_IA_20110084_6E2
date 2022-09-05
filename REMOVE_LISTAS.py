3c131a9f468878d9c9b46989e7d838a37e003371
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:36:32 -0500

    Practica_13

---
+++ REMOVE_LISTAS.py
@@ -0,0 +1,13 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 13 -Eliminar elementos con remove en listas de python
+#Eliminar cierto elementos de una lista predeterminada con la instruccion remove
+
+print("ACTIVIDAD:")
+print("Lista: rojo, azul, verde, amarillo, marron, lila, negro, rosa, blanco, naranja")
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#Aqui con la intruccion de REMOVE vamos  a ir elminando posiciones y sus elementos que esten
+print("\nVamos a eliminar los colores: amarillo y rojo")
+colores.remove('amarillo')
+colores.remove('rojo')
+print("\nAsi queda la nueva lista:")
+print(colores)
