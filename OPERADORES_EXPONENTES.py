081d160698907b355be0cb9d8acb3907cd17638d
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:36:01 -0500

    Practica_8

---
+++ OPERADORES_EXPONENTES.py
@@ -0,0 +1,8 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 8 - Como calcular exponentes en python
+#Obtener como resultado el numero 1024 usando el numero 2
+print("OPERACION:")
+print("2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2")
+exponente = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+print("Resultado: ",exponente)
+
