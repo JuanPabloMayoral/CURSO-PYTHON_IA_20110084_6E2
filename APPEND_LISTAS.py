aefb07755f1c9eb691f14621b5a500785359884b
JuanPabloMayoral <mayoraljuanpablo21@gmail.com>
2022-09-05 01:31:38 -0500

    Practica_15

---
+++ APPEND_LISTAS.py
@@ -0,0 +1,12 @@
+# Juan Pablo Mayoral López 20110084 6E2
+#Capitulo 15 - Insertar elementos con append() en listas para python
+#Añadir los colores 'fuxia' y 'celeste' a la lista.
+
+print("ACTIVIDAD")
+print("Lista de colores: rojo, azul, verde, amarillo, marron, lila, negro, rosa, blanco, naranja")
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#con la intruccion append vamos añadir dos nuevos colores
+colores.append('fuxia')
+colores.append('celeste')
+print("La nueva lista queda: ")
+print(colores)
